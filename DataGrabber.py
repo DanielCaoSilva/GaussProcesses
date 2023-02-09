3dfd7a4ee29289eedc7c09150e46048a744fbee0
DanielCaoSilva <47014112+DanielCaoSilva@users.noreply.github.com>
2023-02-09 02:43:16 -0800

    added readme and fixed datagrabber module

--- DataGrabber.py
+++ DataGrabber.py
@@ -1,50 +1,87 @@
 import pandas as pd
 from pydap.client import open_url
+import numpy as np
 
 
+# https://dods.ndbc.noaa.gov/thredds/dodsC/data/stdmet/46211/46211h2022.nc
 class DataGrab:
 	base_data_set = r'https://dods.ndbc.noaa.gov/thredds/dodsC/data/stdmet/'
 	raw_dataset = []
 	data_url = []
+	variable_keys = []
+	columns_out = []
 
-	def __init__(self, site_number='46221', year=None, data_type='wave_height'):
-		if year is None:
-			year = ['2022']
+	def __init__(self, site_number='46221', year='2022', data_type='wave_height'):
 		self.site = site_number
 		self.year = year
 		self.data_type = data_type
 
 	def get_urls(self):
-		#if self.year is list:
-		for y in self.year:
-			self.data_url.append(f'{self.base_data_set}{self.site}/{self.site}h{y}.nc')
-		#else:
-			#self.data_url[0] = f'{self.base_data_set}{self.site}/{self.site}h{self.year}.nc'
+		# for y in self.year:
+		self.data_url.append(f'{self.base_data_set}{self.site}/{self.site}h{self.year}.nc')
 
-	def open_url(self):
+	def open_urls(self):
 		for d in self.data_url:
-			self.raw_dataset.append(open_url(d))
+			self.raw_dataset.append(open_url(d, output_grid=False))
 
-	def process_data_wave_height(self):
+	def process_buoy_data(self):
+		# print(self.data_url)
+		self.data_url.append(f'{self.base_data_set}{self.site}/{self.site}h{self.year}.nc')
+		self.raw_dataset = open_url(self.data_url[0], output_grid=False)
+		self.variable_keys = list(self.raw_dataset.keys())
+		# print(self.variable_keys)
+		# for r in self.raw_dataset:
+		# print(self.raw_dataset)
+		columns_out = []
 		total_wave_data = pd.DataFrame()
-		for r in self.raw_dataset:
-			wave_height = r[self.data_type].data[0]
-			time_ = r[self.data_type].data[1]
-			wave_data = pd.DataFrame(time_, columns=['Time'])
-			wave_data[self.data_type] = wave_height
-			wave_data.set_index('Time')
-			total_wave_data = pd.concat([total_wave_data, wave_data])
+		for i in range(len(self.variable_keys)):
+			if (i != 1) and (i != 2):
+				# print(self.variable_keys[i])
+				columns_out.append(self.variable_keys[i])
+				# print(np.array(self.raw_dataset[self.variable_keys[i]][:].data, dtype="<f4").ravel())
+				total_wave_data[self.variable_keys[i]] = np.array(self.raw_dataset[self.variable_keys[i]][:].data, dtype="<f4").ravel()
+				print(total_wave_data)
+			# wave_height = r[self.data_type].data[0]
+			# time_ = r[self.data_type].data[1]
+			# wave_data = pd.DataFrame(time_, columns=['Time'])
+			# wave_data[self.data_type] = wave_height
+			# wave_data.set_index('Time')
+			# total_wave_data = pd.concat([total_wave_data, wave_data])k
+		# total_wave_data.columns = columns_out
 		return total_wave_data
 
 	def grab_data(self):
 		self.get_urls()
-		self.open_url()
-		return self.process_data_wave_height()
+		# self.open_url()
+		return pd.DataFrame(self.process_buoy_data(), columns=self.columns_out).astype(float)
+
+	def get_vars(self):
+		return self.variable_keys
 
 
-# dg = DataGrab(year=["2022", "2021"])
-# wave_dat = dg.grab_data()
-# print(wave_dat)
+# dg = DataGrab(year='9999', site_number='46221', data_type='wave_height')
+# print(dg.grab_data())
+# print("Grab urls")
+# dg.get_urls()
+# print("Open urls")
+# dg.open_url()
+# keys = list(dg.raw_dataset[0].keys())
+# print(keys)
+#
+# df = pd.DataFrame()
+# # print(
+# # 	np.array(dg.raw_dataset[0][keys[0]][:].data, dtype="<f4").ravel()
+# # )
+#
+# # 	# print(df)
+# print(df)
+# # print(dg.raw_dataset[0][keys[3:]].array[:])
+#
+# # print(pd.DataFrame(dg.raw_dataset[0].data[3]))#.data[0]))
+# # print(pd.DataFrame(dg.raw_dataset[0][3][]))#.data[0]))
+# # dg = DataGrab(year=["2022", "2021"])
+# # wave_dat = dg.grab_data()
+# # print(wave_dat)
 
 
 
---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+# Useful Links and Refereces 
+Basic Documentation on the NDBC THREDDS Server: `https://www.ndbc.noaa.gov/docs/ndbc_web_data_guide.pdf`
+Sample Request - with column names listed and brief descriptions: `https://dods.ndbc.noaa.gov/thredds/dodsC/data/stdmet/46211/46211h2022.nc.html`
+
+Column Names that are available to access: 
+- time
+- latitude,
+- longitude
+- wind_dir
+- wind_spd 
+- wave_height 
+- dominant_wpd 
+- average_wpd 
+- mean_wave_dir 
+- air_pressure 
+- air_temperature 
+- sea_surface_temperature 
+- dewpt_temperature 
+- visibility 
+- water_level
