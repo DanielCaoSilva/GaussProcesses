534909cb7ff06f46e9f228230c7666404a96f6ac
DanielCaoSilva <dcsilva@cpp.edu>
2023-06-01 17:28:43 -0700

    Add sys path to bin script

--- bin/run_exact_gp.py
+++ bin/run_exact_gp.py
@@ -7,8 +7,12 @@ import numpy as np
 import pandas as pd
 from datetime import datetime
 from skimage.measure import block_reduce
+from pathlib import Path
 from datetime import datetime
 import gc
+import sys
+import os
+sys.path.append(Path(os.getcwd()).parent.__str__())
 from src.utils import *
 plt.style.use('classic')
 set_gpytorch_settings(False)
@@ -66,107 +70,7 @@ class ExactGPModel(gpytorch.models.ExactGP):
 			.MultivariateNormal(mean_x, covar_x)
 
 
-# Scale the time axis and log transform the Y-values
-X = scaler(X, X_old)
-y = y.log()
-
-# max, min, and scale factor declaration
-scaler_max = X_old.max(0).values.item()
-scaler_min = X_old.min(0).values.item()
-scale_factor = scaler_max - scaler_min
-scaler_consts = [scaler_max, scaler_min, scale_factor]
-print(
-	f'Scale Max: {scaler_max}\n '
-	f'Scale Min: {scaler_min}\n '
-	f'Scale Factor: {scale_factor}')
-temp_for_plotting = pd.Series(
-	using_sk[:-1, 0] * 1e9, dtype='datetime64[ns]')
-plt.plot(temp_for_plotting, using_sk[:-1, 1])
-plt.xlabel("Time (epoch)")
-plt.ylabel("Significant Wave Height (meters)")
-plt.title(f'Significant wave height - after block reducing')
-plt.show()
-
-print(
-	f'Before Block Reduce: {df_as_np.shape}\n'
-	f'After Block Reduce: {using_sk.shape}\n'
-	f'Number of Nans: {np.count_nonzero(np.isnan(df_as_np))}\n'
-	f'Start Time: {datetime.fromtimestamp(df_as_np[0, 0])}\n'
-	f'End Time: {datetime.fromtimestamp(df_as_np[-1, 0])}\n'
-	f'Number of Days: {df_as_np.shape[0] / 48}\n'
-	f'Time Period (Days): {(df_as_np[-1, 0] - df_as_np[0, 0]) / 24 / 60 / 60}')
-
-# Prediction range, training and test set define (14, 3, 365)
-predict_days_out = 28
-test_n = 2 * predict_days_out
-
-# Split the data into train and test sets
-# *contiguous means they are sitting next to each other in memory*
-train_x = X[test_n:].contiguous().cuda()
-train_y = y[test_n:].contiguous().cuda()
-test_x = X[-test_n:].contiguous().cuda()
-test_y = y[-test_n:].contiguous().cuda()
-
-# Create a list of random starting indices for the subtest sets
-n_total = train_x.shape[0]
-idx_list = np.random.randint(
-	low=n_total/2, high=n_total-test_n, size=100)
-
-# Generate the train_loader and train_dataset
-train_loader, train_dataset, test_loader, test_dataset = create_train_loader_and_dataset(
-	train_x, train_y, test_x, test_y)
-data_compact = [
-	train_x, train_y, test_x, test_y,
-	train_loader, train_dataset,
-	test_loader, test_dataset]
-
-# List of possible Kernels operations
-kernel_operations = ["+", "*"]
-
-# List of possible Kernels terms
-kernel_list = [
-	# Smoothing Kernels of the Matern class
-	"RBF", "Mat_2.5", "Mat_1.5", "Mat_0.5",
-	# Random Fourier Features Kernel
-	# "RFF",
-	# Varying Length Scales of the RBF Kernel
-	"RQ",
-	# Speciality Kernels
-	"AR2", "Min",
-	# Periodic Kernels of Varying Period constraints
-	"Per_Arb", "Per_Year", "Per_Season", "Per_Month", "Per_Week"  # "Per_Unbounded"]
-]
-# kernel_list = ["Per_Arb*RBF", "Per_Month*RBF", "Per_Year*RBF", "Per_Week*RBF"]
-
-# Initial Kernel Trial
-# kernel_str_running = "RBF+Per_Month*RBF+AR2*RFF+Per_Year"
-# kernel_str_running = "RBF+Per_Month*RBF"
-kernel_str_running = "RQ"
-# kernel_str_running = "RFF"
-# kernel_str_running = "AR2*RFF"
-# kernel_str_running = "AR2*RFF+RQ"
-# kernel_str_running = "RBF+Mat_2.5*Per_Arb"
-
-# Initializing empty list to record values
-save_history = []
-bic_values = [10000]
-n = 0
-initial_learning_rate = 0.01  # 0.0063 # initial learning rate
-with_and_without_scheduler = [True]
-search_params = {
-	"possible_kernel_list": kernel_list,
-	"possible_kernel_operations": kernel_operations,
-	"kernel_str_running_start": kernel_str_running,
-	"bic_values_list": bic_values,
-	"data": data_compact,
-	"scale": scaler_consts,
-	"outer_count": 0,
-	"initial_lr": initial_learning_rate,
-	"epoch_iter": 1000
-}
-
-
-# Kernel Search Function - search_for_min_BIC()
+# Breath-Level Kernel Search Function - search_for_min_BIC()kkk
 def search_for_min_BIC(
 		possible_kernel_list, possible_kernel_operations,
 		kernel_str_running_start, bic_values_list,
@@ -256,7 +160,8 @@ def search_for_min_BIC(
 	return kernel_str_running_start, trial_hist, potential_best_kernel
 
 
-def save_results():
+# Depth-Level search Function and save results
+def save_results(search_params):
 	full_path_history = []
 	today_string = datetime.today().strftime('%Y-%m-%d-%H:%M:%S')
 	full_path_history_string = "full_path_history_" + today_string + ".csv"
@@ -279,81 +184,98 @@ def save_results():
 	cleaned_fph_df = clean_csv_saves(full_path_history_string)
 	print(cleaned_fph_df)
 	print("Last kernel found: ", search_params["kernel_str_running_start"])
+	return cleaned_fph_df
 
 
-save_results()
+# Scale the time axis and log transform the Y-values
+X = scaler(X, X_old)
+y = y.log()
 
+# max, min, and scale factor declaration
+scaler_max = X_old.max(0).values.item()
+scaler_min = X_old.min(0).values.item()
+scale_factor = scaler_max - scaler_min
+scaler_consts = [scaler_max, scaler_min, scale_factor]
+temp_for_plotting = pd.Series(
+	using_sk[:-1, 0] * 1e9, dtype='datetime64[ns]')
+plt.plot(temp_for_plotting, using_sk[:-1, 1])
+plt.xlabel("Time (epoch)")
+plt.ylabel("Significant Wave Height (meters)")
+plt.title(f'Significant wave height - after block reducing')
+plt.show()
 
-# found_kernel, path_history = search_for_min_BIC(
-# 	kernel_list, kernel_operations, kernel_str_running,
-# 	bic_values, data_compact, scaler_consts, 0, initial_learning_rate, 1000)
+print(
+	f'Scale Max: {scaler_max}\n '
+	f'Scale Min: {scaler_min}\n '
+	f'Scale Factor: {scale_factor}'
+	f'Before Block Reduce: {df_as_np.shape}\n'
+	f'After Block Reduce: {using_sk.shape}\n'
+	f'Number of Nans: {np.count_nonzero(np.isnan(df_as_np))}\n'
+	f'Start Time: {datetime.fromtimestamp(df_as_np[0, 0])}\n'
+	f'End Time: {datetime.fromtimestamp(df_as_np[-1, 0])}\n'
+	f'Number of Days: {df_as_np.shape[0] / 48}\n'
+	f'Time Period (Days): {(df_as_np[-1, 0] - df_as_np[0, 0]) / 24 / 60 / 60}')
 
-# print(found_kernel)
-# print(path_history)
+# Prediction range, training and test set define (14, 3, 365)
+predict_days_out = 28
+test_n = 2 * predict_days_out
 
-# exact_gp_obj = TrainTestPlotSaveExactGP(
-# 	ExactGPModel,
-# 	kernel=kernel_str_running,
-# 	train_x=data_compact[0], train_y=data_compact[1], test_x=data_compact[2], test_y=data_compact[3],
-# 	scaler_min=scaler_consts[1], scaler_max=scaler_consts[0],
-# 	num_iter=5,
-# 	lr=0.01,  # lr=0.0063, #lr=0.01,
-# 	name=kernel_str_running,
-# 	save_loss_values="save",
-# 	use_scheduler=True)
-# exact_gp_obj.run_train_test_plot_kernel(set_xlim=[0.96, 1])
+# Split the data into train and test sets
+# *contiguous means they are sitting next to each other in memory*
+train_x = X[test_n:].contiguous().cuda()
+train_y = y[test_n:].contiguous().cuda()
+test_x = X[-test_n:].contiguous().cuda()
+test_y = y[-test_n:].contiguous().cuda()
 
+# Create a list of random starting indices for the subtest sets
+n_total = train_x.shape[0]
+idx_list = np.random.randint(
+	low=n_total/2, high=n_total-test_n, size=100)
 
-# for i in range(2):
-# 	new_kernel_best = kernel_str_running
-# 	kernel_str_running, path_history = search_for_min_BIC(
-# 		kernel_list, kernel_operations,
-# 		new_kernel_best, bic_values,
-# 		data_compact, scaler_consts,
-# 		initial_lr=initial_learning_rate, epoch_iter=10)
-# 	save_history.append(path_history)
-# column_names = [
-# 	'n', 'Kernel_Name', 'BIC', 'Hyper_Parameters', 'Kernel']
-# bic_out_df = pd.DataFrame(save_history)#, columns=column_names)
-# # bic_out_df.to_csv('bin_save_history.csv')
-# # print(bic_out_df.iloc[:, 0:2])
-# print(bic_out_df)
+# Generate the train_loader and train_dataset
+train_loader, train_dataset, test_loader, test_dataset = create_train_loader_and_dataset(
+	train_x, train_y, test_x, test_y)
+data_compact = [
+	train_x, train_y, test_x, test_y,
+	train_loader, train_dataset,
+	test_loader, test_dataset]
 
-# Search through possible composite kernel combinations for a better BIC value
-# while bic_values[-1] > -2500:
-# 	for ops_index, iter_ops in enumerate(kernel_operations):
-# 		for kernel_term_index, iter_kernel_terms in enumerate(kernel_list):
-# 			if n == 0:
-# 				kernel_str = kernel_str_running
-# 			else:
-# 				kernel_str = add_new_kernel_term(
-# 					kernel_str_running, iter_kernel_terms, iter_ops)
-# 			exact_gp = TrainTestPlotSaveExactGP(
-# 				ExactGPModel,
-# 				kernel=kernel_str,
-# 				train_x=train_x, train_y=train_y, test_x=test_x, test_y=test_y,
-# 				scaler_min=scaler_min, scaler_max=scaler_max,
-# 				num_iter=1000,
-# 				lr=initial_learning_rate,  # lr=0.0063, #lr=0.01,
-# 				name=kernel_str,
-# 				save_loss_values="save",
-# 				use_scheduler=True)
-# 			current_bic_value, hyper_values = exact_gp.run_train_test_plot_kernel(set_xlim=[0.96, 1])
-# 			print("Iterations Number(n): ", n, "Learning Rate: ", initial_learning_rate)
-# 			print("Kernel Structure (Old Best): ", kernel_str_running, "\n BIC: ", bic_values[-1])
-# 			print("Kernel Structure (Current Trial): ", kernel_str, "\n BIC: ", current_bic_value)
-# 			if current_bic_value < bic_values[-1]:
-# 				bic_values.append(current_bic_value)
-# 				kernel_str_running = kernel_str
-# 			bic_save.append([
-# 				n, kernel_str,
-# 				current_bic_value,
-# 				hyper_values,
-# 				exact_gp.kernel])
-# 			n += 1
-# 			del exact_gp
-# 			gc.enable()
-# 			gc.collect()
-# 			torch.cuda.empty_cache()
+# List of possible Kernels operations
+kernel_operations = ["+", "*"]
 
+# List of possible Kernels terms
+kernel_list = [
+	# Periodic Kernels of Varying Period constraints
+	"Per_Arb", "Per_Year", "Per_Season", "Per_Month", "Per_Week"  # "Per_Unbounded"]
+	# Random Fourier Features Kernel
+	# "RFF",
+	# Varying Length Scales of the RBF Kernel
+	"RQ",
+	# Speciality Kernels
+	"AR2", "Min",
+	# Smoothing Kernels of the Matern class
+	"RBF", "Mat_2.5", "Mat_1.5", "Mat_0.5",
+]
 
+# Initial Kernel Trial
+kernel_str_running = "Per_Arb"
+
+# Initializing empty list to record values
+save_history = []
+bic_values = [10000]
+n = 0
+initial_learning_rate = 0.01  # 0.0063 # initial learning rate
+with_and_without_scheduler = [True]
+search_parameters = {
+	"possible_kernel_list": kernel_list,
+	"possible_kernel_operations": kernel_operations,
+	"kernel_str_running_start": kernel_str_running,
+	"bic_values_list": bic_values,
+	"data": data_compact,
+	"scale": scaler_consts,
+	"outer_count": 0,
+	"initial_lr": initial_learning_rate,
+	"epoch_iter": 1000
+}
+
+returned_results = save_results(search_parameters)
--- misc/confirmCuda.py
+++ misc/confirmCuda.py
@@ -5,7 +5,75 @@ print(x)
 
 print(torch.cuda.is_available())
 
+# Old Code Snippets to store
+# found_kernel, path_history = search_for_min_BIC(
+# 	kernel_list, kernel_operations, kernel_str_running,
+# 	bic_values, data_compact, scaler_consts, 0, initial_learning_rate, 1000)
+
+# print(found_kernel)
+# print(path_history)
+
+# exact_gp_obj = TrainTestPlotSaveExactGP(
+# 	ExactGPModel,
+# 	kernel=kernel_str_running,
+# 	train_x=data_compact[0], train_y=data_compact[1], test_x=data_compact[2], test_y=data_compact[3],
+# 	scaler_min=scaler_consts[1], scaler_max=scaler_consts[0],
+# 	num_iter=5,
+# 	lr=0.01,  # lr=0.0063, #lr=0.01,
+# 	name=kernel_str_running,
+# 	save_loss_values="save",
+# 	use_scheduler=True)
+# exact_gp_obj.run_train_test_plot_kernel(set_xlim=[0.96, 1])
 
 
+# for i in range(2):
+# 	new_kernel_best = kernel_str_running
+# 	kernel_str_running, path_history = search_for_min_BIC(
+# 		kernel_list, kernel_operations,
+# 		new_kernel_best, bic_values,
+# 		data_compact, scaler_consts,
+# 		initial_lr=initial_learning_rate, epoch_iter=10)
+# 	save_history.append(path_history)
+# column_names = [
+# 	'n', 'Kernel_Name', 'BIC', 'Hyper_Parameters', 'Kernel']
+# bic_out_df = pd.DataFrame(save_history)#, columns=column_names)
+# # bic_out_df.to_csv('bin_save_history.csv')
+# # print(bic_out_df.iloc[:, 0:2])
+# print(bic_out_df)
 
-
+# Search through possible composite kernel combinations for a better BIC value
+# while bic_values[-1] > -2500:
+# 	for ops_index, iter_ops in enumerate(kernel_operations):
+# 		for kernel_term_index, iter_kernel_terms in enumerate(kernel_list):
+# 			if n == 0:
+# 				kernel_str = kernel_str_running
+# 			else:
+# 				kernel_str = add_new_kernel_term(
+# 					kernel_str_running, iter_kernel_terms, iter_ops)
+# 			exact_gp = TrainTestPlotSaveExactGP(
+# 				ExactGPModel,
+# 				kernel=kernel_str,
+# 				train_x=train_x, train_y=train_y, test_x=test_x, test_y=test_y,
+# 				scaler_min=scaler_min, scaler_max=scaler_max,
+# 				num_iter=1000,
+# 				lr=initial_learning_rate,  # lr=0.0063, #lr=0.01,
+# 				name=kernel_str,
+# 				save_loss_values="save",
+# 				use_scheduler=True)
+# 			current_bic_value, hyper_values = exact_gp.run_train_test_plot_kernel(set_xlim=[0.96, 1])
+# 			print("Iterations Number(n): ", n, "Learning Rate: ", initial_learning_rate)
+# 			print("Kernel Structure (Old Best): ", kernel_str_running, "\n BIC: ", bic_values[-1])
+# 			print("Kernel Structure (Current Trial): ", kernel_str, "\n BIC: ", current_bic_value)
+# 			if current_bic_value < bic_values[-1]:
+# 				bic_values.append(current_bic_value)
+# 				kernel_str_running = kernel_str
+# 			bic_save.append([
+# 				n, kernel_str,
+# 				current_bic_value,
+# 				hyper_values,
+# 				exact_gp.kernel])
+# 			n += 1
+# 			del exact_gp
+# 			gc.enable()
+# 			gc.collect()
+# 			torch.cuda.empty_cache()
